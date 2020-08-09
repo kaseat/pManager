1594e667eb8824fbf73fc192195370c3c60055b5
Oleg Petrochenko <nomuur@gmail.com>
2020-08-09 19:47:14 +0300

    fix: price calculation on GetShares

--- storage/mongo/shares.go
+++ storage/mongo/shares.go
@@ -1,8 +1,13 @@
 package mongo
 
 import (
+	"sort"
+	"time"
+
 	"github.com/kaseat/pManager/models"
 	"github.com/kaseat/pManager/models/operation"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // GetShares gets shares
@@ -33,6 +38,47 @@ func (db Db) GetShares(pid string, onDate string) ([]models.Share, error) {
 	for _, sh := range shares {
 		if sh.ISIN != "" && sh.Volume != 0 {
 			result = append(result, sh)
+		}
+	}
+
+	if onDate == "" {
+		onDate = time.Now().Format("2006-01-02T15:04:05Z07:00")
+	}
+
+	if dtime, err := time.Parse("2006-01-02T15:04:05Z07:00", onDate); err == nil {
+		y, m, d := dtime.Date()
+		dt := time.Date(y, m, d, 7, 0, 0, 0, time.UTC)
+
+		and := []interface{}{
+			bson.M{"time": bson.M{"$gte": dt.AddDate(0, 0, -10)}},
+			bson.M{"time": bson.M{"$lte": dt}},
+		}
+		filter := bson.M{"$and": and}
+		findOptions := options.Find()
+		prices, err := db.getPrices(filter, findOptions)
+		if err != nil {
+			return nil, err
+		}
+
+		pricesMap := make(map[string][]models.Price)
+		for _, price := range prices {
+			if pr, ok := pricesMap[price.ISIN]; ok {
+				pr = append(pr, price)
+				pricesMap[price.ISIN] = pr
+			} else {
+				pricesMap[price.ISIN] = []models.Price{price}
+			}
+		}
+
+		pricesMapLastPrice := make(map[string]models.Price)
+		for key, val := range pricesMap {
+			sort.Slice(val, func(i, j int) bool { return val[i].Date.After(val[j].Date) })
+			pricesMapLastPrice[key] = val[0]
+		}
+
+		for i, sh := range result {
+			result[i].Date = dt
+			result[i].Price = pricesMapLastPrice[sh.ISIN].Price
 		}
 	}
 	return result, nil
