2e229af99ac077be1d2937bb0f4f5ce12cd55781
Oleg Petrochenko <nomuur@gmail.com>
2020-08-22 19:57:34 +0300

    fix: primary key on uid

--- db/tables/operations.sql
+++ db/tables/operations.sql
@@ -1,12 +1,17 @@
+CREATE SEQUENCE operations_id_seq;
+
 CREATE TABLE operations (
+    id integer DEFAULT pseudo_encrypt_24(CAST (nextval('operations_id_seq') AS integer)),
     pid integer NOT NULL,
     isin char(12) NOT NULL,
 	time timestamp NOT NULL,
     op_id smallint NOT NULL,
 	vol integer NOT NULL,
 	price numeric(20,6) NOT NULL,
-	CONSTRAINT pk_operations PRIMARY KEY (pid, isin, time),
+	CONSTRAINT pk_operations PRIMARY KEY (id),
     CONSTRAINT fk_operations_users FOREIGN KEY(pid) REFERENCES users(id),
     CONSTRAINT fk_operations_operation_types FOREIGN KEY(op_id) REFERENCES operation_types(id),
     CONSTRAINT fk_operations_securities FOREIGN KEY(isin) REFERENCES securities(isin)
 );
+
+CREATE INDEX ix_operations ON operations(pid, isin, time);
