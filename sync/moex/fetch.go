11405026157b6aa3bd10ad4d7438b75dde7e9640
Oleg Petrochenko <nomuur@gmail.com>
2020-09-15 19:31:19 +0300

    add: sync MOEX prices

---
+++ sync/moex/fetch.go
@@ -0,0 +1,81 @@
+package moex
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/kaseat/pManager/models/currency"
+)
+
+type priceInt struct {
+	Currency currency.Type
+	Date     time.Time
+	Price    float32
+	Volume   int
+}
+
+func fetchFromAPI(client *http.Client, ticker string, cursor int) ([]priceInt, int, error) {
+	columns := "history.columns=BOARDID,TRADEDATE,LEGALCLOSEPRICE,VOLUME"
+	from := "from=2019-05-01"
+	start := fmt.Sprintf("start=%d", cursor)
+	url := fmt.Sprintf("http://iss.moex.com/iss/history/engines/stock/markets/shares/securities/%s.json?iss.meta=off", ticker)
+	url = fmt.Sprintf("%s&%s&%s&%s", url, columns, from, start)
+
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+
+	r, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, 0, err
+	}
+
+	var rawResponse struct {
+		History struct {
+			Data [][]interface{} `json:"data"`
+		} `json:"history"`
+		Cursor struct {
+			Data [][]float32 `json:"data"`
+		} `json:"history.cursor"`
+	}
+
+	err = json.Unmarshal(r, &rawResponse)
+	if err != nil {
+		fmt.Println(err)
+		return nil, 0, err
+	}
+
+	prices := make([]priceInt, len(rawResponse.History.Data))
+
+	for i, rawPrice := range rawResponse.History.Data {
+		var curr currency.Type
+		if rawPrice[0] == "TQTD" {
+			curr = currency.USD
+		} else {
+			curr = currency.RUB
+		}
+
+		dtime, _ := time.Parse("2006-01-02", rawPrice[1].(string))
+
+		price := priceInt{
+			Currency: curr,
+			Date:     dtime,
+			Price:    float32(rawPrice[2].(float64)),
+			Volume:   int(rawPrice[3].(float64)),
+		}
+		prices[i] = price
+	}
+	if int(rawResponse.Cursor.Data[0][1])-cursor-100 > 0 {
+		cursor += 100
+	} else {
+		cursor = 0
+	}
+	return prices, cursor, nil
+}
---
+++ sync/moex/sync.go
@@ -0,0 +1,43 @@
+package moex
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+var isSync int32
+
+// Sync starts moex sync
+func Sync(ticker string) {
+	defer func() {
+		atomic.StoreInt32(&isSync, 0)
+		fmt.Println(time.Now().Format("2006-02-01 15:04:05"), "End sync MOEX")
+	}()
+	fmt.Println(time.Now().Format("2006-02-01 15:04:05"), "Begin sync MOEX")
+	if atomic.LoadInt32(&isSync) == 1 {
+		err := errors.New("MOEX sync already in process")
+		fmt.Println(time.Now().Format("2006-02-01 15:04:05"), "Error sync instruments:", err)
+		return
+	}
+
+	atomic.StoreInt32(&isSync, 1)
+	client := &http.Client{}
+	var prices []priceInt
+
+	cur := 0
+	for {
+		pr, curRe, err := fetchFromAPI(client, ticker, cur)
+		if err != nil {
+			break
+		}
+		prices = append(prices, pr...)
+		if curRe == 0 {
+			break
+		}
+		cur = curRe
+	}
+	fmt.Println(len(prices))
+}
