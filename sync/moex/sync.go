dc4e36508f9ded982748080a0ac2a5c584acb61d
Oleg Petrochenko <nomuur@gmail.com>
2020-09-19 19:44:05 +0300

    fix: MOEX sync

--- sync/moex/sync.go
+++ sync/moex/sync.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"sync/atomic"
 	"time"
+
+	"github.com/kaseat/pManager/storage"
 )
 
 var isSync int32
@@ -22,24 +24,35 @@ func Sync(ticker string) {
 		fmt.Println(time.Now().Format("2006-02-01 15:04:05"), "Error sync instruments:", err)
 		return
 	}
-
 	atomic.StoreInt32(&isSync, 1)
+
 	client := &http.Client{}
-	var prices []priceInt
 
-	from := time.Date(2019, 5, 10, 0, 0, 0, 0, time.UTC)
-
-	cur := 0
-	for {
-		pr, curRe, err := fetchFromAPI(client, from, ticker, cur)
-		if err != nil {
-			break
-		}
-		prices = append(prices, pr...)
-		if curRe == 0 {
-			break
-		}
-		cur = curRe
+	s := storage.GetStorage()
+	ins, err := s.GetInstruments("code", "MOEX")
+	if err != nil {
+		fmt.Println(time.Now().Format("2006-02-01 15:04:05"), "Error get securities list:", err)
+		return
 	}
-	fmt.Println(len(prices))
+	fmt.Println(ins)
+	
+	for _, i := range ins {
+		from := i.PriceUptdTime
+		if from.IsZero() {
+			from = time.Date(2019, time.May, 1, 0, 0, 0, 0, time.UTC)
+		}
+		var prices []priceInternal
+		cur := 0
+		for {
+			pr, curRe, err := fetchFromAPI(client, from, ticker, cur)
+			if err != nil {
+				break
+			}
+			prices = append(prices, pr...)
+			if curRe == 0 {
+				break
+			}
+			cur = curRe
+		}
+	}
 }
